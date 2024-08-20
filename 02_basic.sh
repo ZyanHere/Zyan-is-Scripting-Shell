233498cbebe74a487e070d27a20c0c0f3b25cb26
ZyanHere <craftzyan@gmail.com>
2024-08-20 19:35:02 +0530

    understanding variables

---
+++ 02_basic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# variables
+
+a=10
+name="Zyan"
+age=24
+
+echo "My name is $name and age is $age"
+
+name="ZyanHere"
+
+echo " My new name is $name"
+
+
+#const var
+readonly COLLEGE="Bajali"
